f0e617f2e59650f919fc0bb91c8226331ceee737
robin lanckman <RobinLanckman@hotmail.com>
2020-04-25 16:30:44 +0200

    First Push from local

---
+++ admin/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost:3308');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'blog');
+
+?>
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,92 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">BLOG OP</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            <span>Users</span>
+            <i class="fas fa-user"></i>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+
+                <a class="collapse-item" href="user.php">All Users</a>
+                <a class="collapse-item" href="createUser.php">Create User</a>
+                <a class="collapse-item" href="editUser.php">Edit User</a>
+                <a class="collapse-item" href="deleteUser.php">Delete User</a>
+            </div>
+        </div>
+    </li>
+
+
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+
+            <span>Upload</span>
+            <i class="fas fa-upload"></i>
+        </a>
+    </li>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+
+            <span>Foto's</span>
+            <i class="fas fa-camera"></i>
+        </a>
+    </li>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+
+            <span>Comments</span>
+            <i class="fas fa-comments"></i></a>
+    </li>
+
+
+
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+
+            <span>Tables</span>
+            <i class="fas fa-fw fa-table"></i>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
