2da1a90cda8f692d7d8f7ed3f52fc93823ef2d48
robin <robinlanckman@hotmail.com>
2020-05-26 15:26:58 +0200

    .

--- admin/photos.php
+++ admin/photos.php
@@ -1,6 +1,12 @@
-<?php include ("includes/header.php"); ?>
+<?php include ("includes/header.php");
+
+if(!$session->is_signed_in()){
+    redirect('login.php');
+}
+$photos = Photo::find_all();
+
+?>
 <?php include ("includes/sidebar.php"); ?>
 <?php include ("includes/content-top.php"); ?>
-<?php include ("includes/content.php"); ?>
 <?php include ("includes/footer.php"); ?>
 
